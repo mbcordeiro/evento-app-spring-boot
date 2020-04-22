f1e283bffc9972f81a1dc5bf6af6881aad8aa5cf
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-21 23:18:58 -0300

    DB Config

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,6 +1,21 @@
 package com.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long codigo;
+	
 	private String nome;
 	private String local;
 	private String data;
@@ -11,6 +26,12 @@ public class Evento {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
 	}
 	public String getLocal() {
 		return local;
